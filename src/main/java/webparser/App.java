6434e8e3ff1c4f06657aaf8882d02da61dbb1415
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2023-12-13 04:21:54 +0100

    feat: Jsop for POE website

--- src/main/java/webparser/App.java
+++ src/main/java/webparser/App.java
@@ -1,5 +1,19 @@
 package webparser;
 
-public class App {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
+import org.jsoup.select.Elements;
+
+public class App {
+  public static void main(String[] args) throws IOException {
+    Document page = Jsoup.connect("https://www.pathofexile.com/").get();
+
+    Elements titles  = page.selectXpath("//*[@class='newsListItem alt']/h2/a");
+
+    String href = titles.attr("href");
+
+    System.out.println(titles.text() +"\n"+ href);
+  }
 }
